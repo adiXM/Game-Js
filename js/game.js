abb7965dfe22cec07e5280498a8a58f6d720a12c
adiXM <gemene_adrian@yahoo.com>
2017-02-12 01:18:17 +0200

    refactor

--- js/game.js
+++ js/game.js
@@ -1,110 +1,102 @@
 	
-		var c=document.getElementById("start");
-		var ctx=c.getContext("2d");
+		var c = document.getElementById("start");
+		var ctx = c.getContext("2d");
 		ctx.strokeStyle = 'black';
 		ctx.strokeRect(0, 0, 750, 360);
 		ctx.font="50px Arial";
 		ctx.fillText("Snake Game",220,180);
+
+		var KEYCODE_LEFT = 37;
+		var KEYCODE_RIGHT = 39;
+		var KEYCODE_UP = 38;
+		var KEYCODE_DOWN = 40;
+
+		scena = document.getElementById('start');
+		game = scena.getContext("2d");
+
+
+		score = 0, best = 0;
+		snakeSize = 15, food = 0;
+		width = 750, height = 360;
+		snakeX = 1, snakeY = 1, dir = 'right';
+
+		snake = [];
+
+		var Make_Snake = function () {
+			snake = [];
+			snake.push({x : 1, y : 1});			
+		};
 	
-		function Start_Game() {
-				document.getElementById("but").disabled = true;
-				var scena = document.getElementById('start');
-				var game = scena.getContext("2d");
-				var snake, score = 0;
-				var snakeSize = 15, food;
-				var width = 750, height = 360;
-				var snakex = 1, snakey = 1, dir, best = 0;
-				var Celula = function(x, y) {
-				        game.fillStyle = '#4CAF50';
-				        game.fillRect(x*snakeSize, y*snakeSize, snakeSize, snakeSize);      
-				};
-				var Make_Snake = function () {
-					snake = [];
-					snake.push({x : 1, y : 1});
-				};
-				function Reseteaza() {
-					snakeSize = 15;
-					width = 750, height = 360;
-					snakex = 1, snakey = 1;
+		var Make_Food = function () {
+			var found;
+			do {
+				found = false;
+				food = {
+					x: Math.floor((Math.random() * 49) + 1),
+	        		y: Math.floor((Math.random() * 22) + 1)
 				}
-				var Make_Food = function () {
-					food = {
-						x: Math.floor((Math.random() * 49) + 1),
-        				y: Math.floor((Math.random() * 22) + 1)
+				for (var i = 0; i < snake.length && found == false; i++) {
+					if(snake[i].x == food.x && snake[i].y == food.y) {
+						found = true;
 					}
-					for (var i = 0; i < snake.length; i++) {
-						if(snake[i].x == food.x && snake[i].y == food.y) {
-							food.x = Math.floor((Math.random() * 49) + 1);
-        					food.y = Math.floor((Math.random() * 22) + 1);
-						}
-					};
 				};
-				
-				var Debug = function () {
-					for (var i = 0; i < snake.length; i++) {
-						console.log(snake[i].x);
-						console.log(snake[i].y);
-					};
-					
-				}
-				var Touch = function (x, y, a) {
-					for (var i = 0; i < a.length; i++) {
-						if(a[i].x == x  && a[i].y == y) {
-							return 1;
-						}
-					};
-					return 0;
-				}
-				var Run_Game = function () {
+			}
+			while(found == true);
+		};
 
-					game.fillStyle = 'white';
-				    game.fillRect(0, 0, width, height);
-				    game.strokeStyle = 'black';
-				    game.strokeRect(0, 0, width, height);
-				    document.addEventListener('keydown', Miscari);
-				   	document.getElementById("scor").innerHTML = "Score: " + score;
-				   	if(score > best) {
-					   	best = score;
-					}
-					function Miscari(e) {
-					  var x = e.keyCode;
-		
-					    if(x == 37) {
-					    	if(dir != 'right') {
-					    		dir = 'left';
-					    	}
-					    }
-					    if(x == 39) {
-					    	if(dir != 'left') {
-					    		dir = 'right';
-					    	}
-					    }
-					    if(x == 38) {
-					    	if(dir != 'down') {
-					    		dir = 'up';
-					    	}
-					    }
-					    if(x == 40) {
-					    	if(dir != 'up') {
-					    		dir = 'down';
-					    	}
-					    }
-					  //console.log(x);
-					}
+		var DrawCell = function(x, y) {
+			game.fillStyle = '#4CAF50';
+			game.fillRect(x * snakeSize, y * snakeSize, snakeSize, snakeSize);      
+		};
 
+		var Run_Game = function () {
+
+			game.fillStyle = 'white';
+		    game.fillRect(0, 0, width, height);
+		    game.strokeStyle = 'black';
+			game.strokeRect(0, 0, width, height);
+			document.addEventListener('keydown', function (e) {
+
+				var keyPressed = e.keyCode;
+			    if(keyPressed == KEYCODE_LEFT) {
+				   	if(dir != 'right') {
+				   		dir = 'left';
+				   	}
+				}
+				if(keyPressed == KEYCODE_RIGHT) {
+				   	if(dir != 'left') {
+				   		dir = 'right';
+				   	}
+				}
+				if(keyPressed == KEYCODE_UP) {
+				  	if(dir != 'down') {
+				   		dir = 'up';
+				   	}
+				}
+				if(keyPressed == KEYCODE_DOWN) {
+				  	if(dir != 'up') {
+				   		dir = 'down';
+				   	}
+				}
+			});
+
+			document.getElementById("scor").innerHTML = "Score: " + score;
+			best = Math.max(best, score);
+			
 					if(dir == "right") {
-						snakex++;
+						snakeX++;
 					}
 					else if(dir == 'left') {
-						snakex--;
+						snakeX--;
 					}
 					else if(dir == 'up') {
-						snakey--;
+						snakeY--;
 					}
 					else if(dir == 'down') {
-						snakey++;
+						snakeY++;
 					}
-					if(snakex == -1 || snakex == width/snakeSize || snakey == -1 || snakey == height/snakeSize || Touch(snakex, snakey, snake) ) {
+					if(Lose() == true)
+					{
 						document.getElementById("bestscor").innerHTML = "Best Score: " + best;
 					    if (confirm("Mai joci?") == true) {
 					      	game.clearRect(0,0,width,height);
@@ -112,35 +104,46 @@
           					Reseteaza();
           					Init();
 					    } else {
-					    	game.clearRect(0,0,width,height);
-					    	game.fillStyle = 'white';
-				    		game.fillRect(0, 0, width, height);
-				    		game.strokeStyle = 'black';
-				    		game.strokeRect(0, 0, width, height);
-				    		game.font="50px Arial";
-				    		game.fillStyle = "black"
-					    	game.fillText("Game over!",220,180);
-					    	document.getElementById("but").disabled = false;
-							document.getElementById("scor").innerHTML = "";
+					    	ClearBoard();
 					    	Run = clearInterval(Run);
 					    	return;
 					    }
 					}
-					var front = {x: snakex, y: snakey}
-					if(snakex == food.x && snakey == food.y) {
+					var front = {x: snakeX, y: snakeY}
+					if(snakeX == food.x && snakeY == food.y) {
 						score++;
 						Make_Food();
 					}
 					else {
 						snake.pop();
-						front.x = snakex;
-						front.y =  snakey;
+						front.x = snakeX;
+						front.y =  snakeY;
 					}
 					snake.unshift(front);
 					for (var i = 0; i < snake.length; i++) {
-						Celula(snake[i].x, snake[i].y);
+						DrawCell(snake[i].x, snake[i].y);
 					}
-					Celula(food.x, food.y);
+					DrawCell(food.x, food.y);
+				}
+
+		function Start_Game() {
+				document.getElementById("but").disabled = true;
+				
+				
+				
+				function Reseteaza() {
+					snakeSize = 15;
+					width = 750, height = 360;
+					snakeX = 1, snakeY = 1;
+				}
+				
+				
+				var Debug = function () {
+					for (var i = 0; i < snake.length; i++) {
+						console.log(snake[i].x);
+						console.log(snake[i].y);
+					};
+					
 				}
 
 				var Init = function() {
@@ -152,3 +155,33 @@
 				}
 				Init();
 		};
+
+		function ClearBoard() {
+			game.clearRect(0,0,width,height);
+			game.fillStyle = 'white';
+			game.fillRect(0, 0, width, height);
+			game.strokeStyle = 'black';
+			game.strokeRect(0, 0, width, height);
+			game.font="50px Arial";
+			game.fillStyle = "black"
+			game.fillText("Game over!",220,180);
+			document.getElementById("but").disabled = false;
+			document.getElementById("scor").innerHTML = "";
+		}
+
+		var Touch = function () {
+			for (let pos in snake) {
+				if(snake[pos].x == snakeX  && snake[pos].y == snakeY) {
+					return true;
+				}
+			};
+			return false;
+		}
+
+		function Lose() {
+			if(snakeX == -1 || snakeX == width/snakeSize || snakeY == -1 || snakeY == height/snakeSize || Touch() == true) {
+				return true;
+			} 
+			return false;
+
+		}
